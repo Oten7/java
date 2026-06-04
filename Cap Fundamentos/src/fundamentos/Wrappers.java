c47d41bae3b3ce09707ae6b9d016fbbce4933436
Oten7 <netinhogames3@gmail.com>
2026-06-04 20:10:05 -0300

    vida

---
+++ Cap Fundamentos/src/fundamentos/Wrappers.java
@@ -0,0 +1,39 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Wrappers {
+    public static void main(String[] args) {
+
+        //Byte
+
+//        Scanner entrada = new Scanner(System.in);
+        Byte b = 100;
+        Short s = 1000;
+
+        // Integer.parseInt(entrada.next()); // int
+
+        Integer i = 100000;
+        Long l = 10000000L;
+
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i * 3);
+
+        Float f = 123.0F;
+        System.out.println(f);
+
+        Double d = 1234.5678;
+        System.out.println(d);
+
+        Boolean bo = Boolean.parseBoolean("true");
+        System.out.println(bo);
+        System.out.println(bo.toString().toUpperCase());
+
+        Character c = '#'; // char
+        System.out.println(c.toString());
+
+
+
+    }
+}
